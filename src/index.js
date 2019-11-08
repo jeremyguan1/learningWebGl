40327244ea8e882ba3a82d903a25574148062ad1
Jeremy Guan <jeremyguan1@gmail.com>
2019-11-07 18:48:21 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
+// scene.background = new THREE.Color({ color: 0xffffff });
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.set(0, 0, 1);
+
+const light = new THREE.DirectionalLight(0xffffff);
+scene.add(light);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -16,8 +20,14 @@ document.body.appendChild(renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+
+  camera.position.x += 0.01;
+  camera.position.z += 0.01;
+
+  camera.lookAt(scene.position);
+
   renderer.render(scene, camera);
 }
 animate();
